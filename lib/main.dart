e431243f55e508a3b3c077e1cddc4a6e95107093
Heitor de Camargo Azevedo <142464152+HektorTI@users.noreply.github.com>
2024-01-12 00:08:27 -0400

    splash screen page add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'view/home_page_todolist.dart';
+import 'package:mvc_todo_list/view/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo List MVC',
       theme: ThemeData(),
       debugShowCheckedModeBanner: false,
-      home: const TodoListView(),
+      home: const SplashScreen(),
     );
   }
 }
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:mvc_todo_list/view/home_page_todolist.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 5),
+      () => Navigator.of(context).pushReplacement(
+        // Navega para a tela principal após a duração
+        MaterialPageRoute(builder: (context) => const TodoListView()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bem-vindo ao Todo List MVC', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 98, 102, 100),
+      ),
+      backgroundColor: const Color.fromARGB(255, 23, 139, 201),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/splash_image.png',
+              width: 300,
+              height: 300,
+            ),
+            const SizedBox(height: 90.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
